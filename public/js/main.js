cd23cea58f941092b5c77cf719f8b5ac4f783a51
JOB <job.klenchys.i@gmail.com>
2020-04-12 12:57:52 +0200

    Added first-slider

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function () {
-   
+    // $(document).mouseup(function (e) {
+    //     var container = $(".header__form-search input");
+    //     if (container.has(e.target).length === 0){
+    //         container.hide();
+    //     }
+    // });
+    $('.icon-search--click').on('click', function () {
+        $(this).prev().children().toggleClass('header__form-search--open');
+        $('.header__form-search').slideToggle();
+    });
+
+    $('.main-li a').on('click', function (e) {
+        e.preventDefault();
+        $(this).toggleClass('main-li--open');
+        $(this).next().slideToggle();
+    });
+
+
+    $('.header__burger').on('click', function () {
+        $(this).toggleClass('header__burger--open');
+        $('.header__nav').slideToggle();
+    })
 });
